dd4b65fdf5a39d91a31940ff9644fe04ed445f84
Dana Woodman <dana@danawoodman.com>
2017-01-23 17:32:40 -0800

    Add custom env config mapping.

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: 'NODE_ENV',
+  port: 'PORT',
+  numMonths: 'NUMBER_OF_MONTHS',
+  cobotToken: 'COBOT_TOKEN',
+  cobotSubdomain: 'COBOT_SUBDOMAIN',
+  cobotClientId: 'COBOT_CLIENT_ID',
+  cobotClientSecret: 'COBOT_CLIENT_SECRET',
+  refreshInterval: 'REFRESH_INTERVAL',
+}
